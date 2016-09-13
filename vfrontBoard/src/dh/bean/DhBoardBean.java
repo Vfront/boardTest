1b38dc15d844a72ea1c92ed3dbedc527a594e5eb
trumenshow <trumenshow@naver.com>
2016-09-13 17:39:32 +0900

    latest_update_2016_09_13

---
+++ vfrontBoard/src/dh/bean/DhBoardBean.java
@@ -0,0 +1,55 @@
+package dh.bean;
+
+public class DhBoardBean {
+
+	private String board_id;
+	private String board_date;
+	private String user_id;
+	private String title;
+	private String board_content;
+	private String group_num;
+	
+	
+	public String getBoard_id() {
+		return board_id;
+	}
+	public void setBoard_id(String board_id) {
+		this.board_id = board_id;
+	}
+	public String getBoard_date() {
+		return board_date;
+	}
+	public void setBoard_date(String board_date) {
+		this.board_date = board_date;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBoard_content() {
+		return board_content;
+	}
+	public void setBoard_content(String board_content) {
+		this.board_content = board_content;
+	}
+	public String getGroup_num() {
+		return group_num;
+	}
+	public void setGroup_num(String group_num) {
+		this.group_num = group_num;
+	}
+	
+	
+	
+	
+	
+	
+}
